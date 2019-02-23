91b7e0ffb09925beddc69e2ec14a5cdd7e962fe0
Katie Hrenchir <katie.hrenchir@gmail.com>
2019-02-23 11:17:30 -0600

    Adds tmp and dst functionality

--- photobooth.py
+++ photobooth.py
@@ -1,32 +1,55 @@
 from picamera import PiCamera
+from PIL import Image, ImageDraw, ImageFont
 from time import sleep
 import subprocess
 
 camera = PiCamera()
 camera.rotation = 180
-camera.resolution = (600, 600)
+camera.resolution = (800, 800)
 
-camera.start_preview()
+ffmpeg_command = "ffmpeg -framerate 5 -f image2 -i /home/pi/Desktop/kuwic-expo-photobooth/tmp/image%03d.jpg /home/pi/Desktop/kuwic-expo-photobooth/dest/$(date +\"%Y_%m_%d_%I_%M_%p\")_KUWIC_PhotoBooth.gif"
+clear_dir_command = "rm -r /home/pi/Desktop/kuwic-expo-photobooth/tmp/*"
 
-while True:
-    try:
-        # Countdown process
-        for x in range(1,4):
-            camera.capture('/home/pi/Desktop/PhotoBooth/tmp/image%03d.jpg' % x)
-            # Add display of previous image (image00(x-1))
-            # Sleep 3 ish seconds - display countdown, or too busy?
+    
+def countdown():
+    #Display 3
+    sleep(1)
+    #Display 2
+    sleep(1)
+    #Display 1
+    sleep(1)
 
-        # Compile everything currently in tmp into a gif in a destination directory
-        # New gif filename will start with the exact time the gif was compiled
-        subprocess.run("ffmpeg -framerate 5 -f image2 -i /home/pi/Desktop/PhotoBooth/tmp/image%03d.jpg ../dest/$(date +\"%Y_%m_%d_%I_%M_%p\")_KUWIC_PhotoBooth.gif")
+try:
+    camera.start_preview()
+    camera.preview_alpha = 128
 
-        # Remove the photos in tmp for the next run
-        subprocess.run("rm -r /home/pi/Desktop/PhotoBooth/tmp/*")
-        
-    except KeyboardInterrupt:
-        camera.stop_preview()
+    # Countdown process
+    countdown()
+    
+    # Camera will capture 4 photos
+    for x in range(1,5):
+        camera.capture('/home/pi/Desktop/kuwic-expo-photobooth/tmp/image%03d.jpg' % x)
+        print("Captured photo " + str(x) + "!")
+        im = Image.open("tmp/image00" + str(x) + ".jpg")
+        im.show()
+        # Add display of previous image (image00(x-1))
+        # Sleep 3 ish seconds - display countdown, or too busy?
+        sleep(2)
 
-        # Remove the photos in tmp
-        # Note to self - should the terminal ask the user if they want this?
-        subprocess.run("rm -r /home/pi/Desktop/PhotoBooth/tmp/*")
-        break
+    camera.stop_preview()
+
+    # Compile everything currently in tmp into a gif in a destination directory
+    # New gif filename will start with the exact time the gif was compiled
+    subprocess.call(ffmpeg_command, shell=True)
+
+    # Remove the photos in tmp for the next run
+    #subprocess.call(clear_dir_command, shell=True)
+    
+except KeyboardInterrupt:
+    camera.stop_preview()
+
+    # Remove the photos in tmp
+    # Note to self - should the terminal ask the user if they want this?
+    subprocess.call(clear_dir_command, shell=True)
+    
+
